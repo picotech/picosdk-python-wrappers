bc98e1ae1b621a4dfcb97f5a27702de23b1cf7aa
neil.hamilton <neil.hamilton@picotech.com>
2018-06-27 12:38:38 +0100

    Add 3000a series block and rapid block examples

---
+++ ps3000aExamples/ps3000aBlockExample.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+import ctypes
+from picosdk.ps3000a import ps3000a as ps
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Takes a buffer of raw adc count values and converts it into milivolts 
+def adc2mV(bufferADC, range, maxADC):
+    
+    channelInputRanges = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000]
+    vRange = channelInputRanges[range]
+    bufferV = [(x * vRange) / maxADC.value for x in bufferADC]
+
+    return bufferV
+
+# Create chandle and status ready for use
+status = {}
+chandle = ctypes.c_int16()
+
+# Opens the device/s
+status["openunit"] = ps.ps3000aOpenUnit(ctypes.byref(chandle), None)
+
+# Displays the serial number and handle 
+print(chandle.value)
+
+# Set up channel A
+# handle = chandle
+# channel = PS3000A_CHANNEL_A = 0
+# enabled = 1
+# coupling type = PS3000A_DC = 1
+# range = PS3000A_10V = 8
+# analogue offset = 0 V
+chARange = 8
+status["setChA"] = ps.ps3000aSetChannel(chandle, 0, 1, 1, chARange, 0)
+
+# Sets up single trigger
+# Handle = Chandle
+# Enable = 1
+# Source = ps3000A_channel_A = 0
+# Threshold = 1024 ADC counts
+# Direction = ps3000A_Falling = 3
+# Delay = 1000
+status["trigger"] = ps.ps3000aSetSimpleTrigger(chandle, 1, 0, 1024, 3, 0, 1000)
+
+# Setting the number of sample to be collected
+preTriggerSamples = 40000
+postTriggerSamples = 40000
+maxsamples = preTriggerSamples + postTriggerSamples
+
+# Gets timebase innfomation
+# Handle = chandle
+# Timebase = 2 = timebase
+# Nosample = maxsamples
+# TimeIntervalNanoseconds = ctypes.byref(timeIntervalns)
+# MaxSamples = ctypes.byref(returnedMaxSamples)
+# Segement index = 0 
+timebase = 2
+timeIntervalns = ctypes.c_float()
+returnedMaxSamples = ctypes.c_int16()
+status["GetTimebase"] = ps.ps3000aGetTimebase2(chandle, timebase, maxsamples, ctypes.byref(timeIntervalns), 1, ctypes.byref(returnedMaxSamples), 0)
+
+# Creates a overlow location for data
+overflow = ctypes.c_int16()
+# Creates converted types maxsamples
+cmaxSamples = ctypes.c_int32(maxsamples)
+
+# Handle = Chandle
+# nSegments = 10
+# nMaxSamples = ctypes.byref(cmaxSamples)
+
+status["MemorySegments"] = ps.ps3000aMemorySegments(chandle, 10, ctypes.byref(cmaxSamples))
+
+# sets number of captures
+status["SetNoOfCaptures"] = ps.ps3000aSetNoOfCaptures(chandle, 10)
+
+# Starts the block capture
+# Handle = chandle
+# Number of prTriggerSamples
+# Number of postTriggerSamples
+# Timebase = 2 = 4ns (see Programmer's guide for more information on timebases)
+# time indisposed ms = None (This is not needed within the example)
+# Segment index = 0 
+# LpRead = None
+# pParameter = None
+status["runblock"] = ps.ps3000aRunBlock(chandle, preTriggerSamples, postTriggerSamples, timebase, 1, None, 0, None, None)
+
+# Create buffers ready for assigning pointers for data collection
+bufferAMax = (ctypes.c_int16 * maxsamples)()
+bufferAMin = (ctypes.c_int16 * maxsamples)()
+
+# Setting the data buffer location for data collection from channel A
+# Handle = Chandle
+# source = ps3000A_channel_A = 0
+# Buffer max = ctypes.byref(bufferAMax)
+# Buffer min = ctypes.byref(bufferAMin)
+# Buffer length = maxsamples
+# Segment index = 0 
+# Ratio mode = ps3000A_Ratio_Mode_None = 0
+status["SetDataBuffers"] = ps.ps3000aSetDataBuffers(chandle, 0, ctypes.byref(bufferAMax), ctypes.byref(bufferAMin), maxsamples, 0, 0)
+
+# Creates a overlow location for data
+overflow = (ctypes.c_int16 * 10)()
+# Creates converted types maxsamples
+cmaxSamples = ctypes.c_int32(maxsamples)
+
+# Checks data collection to finish the capture
+ready = ctypes.c_int16(0)
+check = ctypes.c_int16(0)
+while ready.value == check.value:
+	status["isReady"] = ps.ps3000aIsReady(chandle, ctypes.byref(ready))
+
+# Handle = chandle
+# start index = 0
+# noOfSamples = ctypes.byref(cmaxSamples)
+# DownSampleRatio = 0
+# DownSampleRatioMode = 0
+# SegmentIndex = 0
+# Overflow = ctypes.byref(overflow)
+
+status["GetValues"] = ps.ps3000aGetValues(chandle, 0, ctypes.byref(cmaxSamples), 0, 0, 0, ctypes.byref(overflow))
+
+# Finds the max ADC count 
+# Handle = chandle
+# Value = ctype.byref(maxADC)
+maxADC = ctypes.c_int16()
+status["maximumValue"] = ps.ps3000aMaximumValue(chandle, ctypes.byref(maxADC))
+
+# Converts ADC from channel A to mV
+adc2mVChAMax =  adc2mV(bufferAMax, chARange, maxADC)
+
+
+# Creates the time data 
+time = np.linspace(0, (cmaxSamples.value) * timeIntervalns.value, cmaxSamples.value)
+
+# Plots the data from channel A onto a graph
+plt.plot(time, adc2mVChAMax[:])
+plt.xlabel('Time (ns)')
+plt.ylabel('Voltage (mV)')
+plt.show()
+
+# Stops the scope 
+# Handle = chandle
+status["stop"] = ps.ps3000aStop(chandle)
+
+# Displays the staus returns
+print(status)
+
+# Closes the unit 
+# Handle = chandle 
+ps.ps3000aCloseUnit(chandle)
+
