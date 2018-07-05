19a0d725985912ae658b9e452909059d67bf7209
neil.hamilton <neil.hamilton@picotech.com>
2018-07-05 09:58:24 +0100

    Change the adc2mv conversion function to refer to functions.py for ps4000BlockExample.py and ps5000aBlockExample.py

--- ps5000aExamples/ps5000aBlockExample.py
+++ ps5000aExamples/ps5000aBlockExample.py
@@ -9,23 +9,7 @@ import ctypes
 import numpy as np
 from picosdk.ps5000a import ps5000a as ps
 import matplotlib.pyplot as plt
-
-def adc2mV(bufferADC, range, maxADC):
-    """ 
-        adc2mc(
-                c_short_Array           buffer
-                int                     range
-                c_int32                  maxADC
-                )
-               
-        Takes a buffer of raw adc count values and converts it into milivolts 
-    """
-
-    channelInputRanges = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000]
-    vRange = channelInputRanges[range]
-    bufferV = [(x * vRange) / maxADC.value for x in bufferADC]
-
-    return bufferV
+from picosdk.functions import *
 
 # Create chandle and status ready for use
 chandle = ctypes.c_int16()
