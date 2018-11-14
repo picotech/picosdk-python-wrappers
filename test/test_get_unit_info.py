9752b128134bb58bea4dedf25e64d9aceb66888c
joe.jordan <joe.jordan@picotech.com>
2018-11-14 17:12:21 +0000

    upgrade get_unit_info to return any requested PICO_INFO keys

--- test/test_get_unit_info.py
+++ test/test_get_unit_info.py
@@ -8,7 +8,7 @@ Unit tests for the wrapper function for retrieving device information.
 from __future__ import print_function
 
 from test.test_helpers import DriverTest, drivers_with_device_connected
-from picosdk.library import DeviceNotFoundError, UnitInfo
+from picosdk.library import DeviceNotFoundError, ArgumentOutOfRangeError
 
 
 try:
@@ -36,9 +36,69 @@ class GetInfoTest(DriverTest):
             info = driver.get_unit_info(device)
             device.close()
 
-            self.assertIsInstance(info, UnitInfo)
             self.assertEqual(info.driver, driver)
             self.assertIsInstance(info.variant, STRING_TYPES)
             self.assertIsInstance(info.serial, STRING_TYPES)
+
+        self.run_snippet_and_count_problems(drivers_to_use, test)
+
+    def test_get_info_advanced_success(self):
+        """test_get_info_advanced_success
+        note: test assumes you have set test_helpers.drivers_with_device_connected"""
+        if not drivers_with_device_connected:
+            return
+        drivers_to_use = drivers_with_device_connected
+
+        def test(driver):
+            devices = []
+            try:
+                devices.append(driver.open_unit())
+            except DeviceNotFoundError:
+                return "no device found."
+
+            device = devices.pop()
+            # three keys present on ALL devices.
+            info = driver.get_unit_info(device,
+                                        "PICO_DRIVER_VERSION",
+                                        "PICO_USB_VERSION",
+                                        "PICO_HARDWARE_VERSION")
+            device.close()
+
+            self.assertEqual(len(info), 3)
+            self.assertIsInstance(info.PICO_DRIVER_VERSION, STRING_TYPES)
+            self.assertIsInstance(info.PICO_USB_VERSION, STRING_TYPES)
+            self.assertIsInstance(info.PICO_HARDWARE_VERSION, STRING_TYPES)
+
+        self.run_snippet_and_count_problems(drivers_to_use, test)
+
+    def test_get_info_with_invalid_key_fails(self):
+        """test_get_info_advanced_success
+        note: test assumes you have set test_helpers.drivers_with_device_connected"""
+        if not drivers_with_device_connected:
+            return
+        drivers_to_use = drivers_with_device_connected
+
+        def test(driver):
+            devices = []
+            try:
+                devices.append(driver.open_unit())
+            except DeviceNotFoundError:
+                return "no device found."
+
+            device = devices.pop()
+            threw = False
+            try:
+                # some valid and some invalid keys. People may pass PICO_CAL_DATE to ps2000 etc.
+                info = driver.get_unit_info(device,
+                                            "PICO_DRIVER_VERSION",
+                                            "aillk_jasgKLASDFG_Jlasdkgfj",
+                                            "PICO_HARDWARE_VERSION")
+            except ArgumentOutOfRangeError:
+                threw = True
+            finally:
+                device.close()
+
+            if not threw:
+                return "get_unit_info didn't throw exception on bad key."
 
         self.run_snippet_and_count_problems(drivers_to_use, test)
