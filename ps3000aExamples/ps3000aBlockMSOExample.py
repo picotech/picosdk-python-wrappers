83896327b363d4372f6a42c25e639dfa4b500b56
hsmistry <hsmistry@users.noreply.github.com>
2018-11-26 17:47:56 +0000

    Initial update to example
    Requires additional work.

--- ps3000aExamples/ps3000aBlockMSOExample.py
+++ ps3000aExamples/ps3000aBlockMSOExample.py
@@ -1,17 +1,21 @@
 #
 # Copyright (C) 2018 Pico Technology Ltd. See LICENSE file for terms.
 #
-# PS3000A BLOCK MODE MSO EXAMPLE
-# This example opens a 3000a driver device, sets up one digital port and a trigger to collect a  block of data.
-# This data is then split into the indivual digital channels and plotted as the binary value against time in ns.
+# PicoScope 3000 Series (A API) MSO Block Mode Example
+# This example demonstrates how to use the PicoScope 3000 Series (ps3000a) driver API functions in order to do the
+# following:
+#
+# Opens a connection to a PicoScope 3000 Series MSO device
+# Setup a digital port
+# Setup a trigger on a digital channel
+# Collect a block of data
+# Plot data
 
 import ctypes
 from picosdk.ps3000a import ps3000a as ps
-from picosdk.functions import splitMSODataPort0, assert_pico_ok
+from picosdk.functions import splitMSOData, assert_pico_ok
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-from array import *
 
 # Gives the device a handle
 status = {}
@@ -41,7 +45,7 @@ except:
 
 # set up digital port
 # handle = chandle
-# PS3000a_DIGITAL_PORT = 0x80
+# PS3000A_DIGITAL_PORT = 0x80
 # Enable = 1
 # logicLevel = 10000
 status["SetDigitalPort"] = ps.ps3000aSetDigitalPort( chandle, 0x80, 1, 10000)
@@ -52,7 +56,7 @@ preTriggerSamples = 400
 postTriggerSamples = 400
 maxsamples = preTriggerSamples + postTriggerSamples
 
-# Gets timebase innfomation
+# Gets timebase information
 # Handle = chandle
 # Timebase = 2 = timebase
 # Nosample = maxsamples
@@ -82,7 +86,8 @@ bufferAMin = (ctypes.c_int16 * maxsamples)()
 # Buffer length = maxsamples
 # Segment index = 0
 # Ratio mode = ps3000A_Ratio_Mode_None = 0
-status["SetDataBuffers"] = ps.ps3000aSetDataBuffers(chandle, 0x80, ctypes.byref(bufferAMax), ctypes.byref(bufferAMin), maxsamples, 0, 0)
+status["SetDataBuffers"] = ps.ps3000aSetDataBuffers(chandle, 0x80, ctypes.byref(bufferAMax), ctypes.byref(bufferAMin),
+                                                    maxsamples, 0, 0)
 assert_pico_ok(status["SetDataBuffers"])
 
 # Starts the block capture
@@ -119,20 +124,20 @@ while ready.value == check.value:
 status["GetValues"] = ps.ps3000aGetValues(chandle, 0, ctypes.byref(cmaxSamples), 0, 0, 0, ctypes.byref(overflow))
 assert_pico_ok(status["GetValues"])
 
-bufferAMaxBinaryD0, bufferAMaxBinaryD1, bufferAMaxBinaryD2, bufferAMaxBinaryD3, bufferAMaxBinaryD4, bufferAMaxBinaryD5, bufferAMaxBinaryD6, bufferAMaxBinaryD7 = splitMSODataPort0(cmaxSamples, bufferAMax)
+bufferDPort0 = splitMSOData(cmaxSamples, bufferAMax)
 
 # Creates the time data
-time = np.linspace(0, (cmaxSamples.value) * timeIntervalns.value, cmaxSamples.value)
+time = np.linspace(0, cmaxSamples.value * timeIntervalns.value, cmaxSamples.value)
 
 # Plots the data from digital channel onto a graph
-plt.plot(time, bufferAMaxBinaryD0[:])
-plt.plot(time, bufferAMaxBinaryD1[:])
-plt.plot(time, bufferAMaxBinaryD2[:])
-plt.plot(time, bufferAMaxBinaryD3[:])
-plt.plot(time, bufferAMaxBinaryD4[:])
-plt.plot(time, bufferAMaxBinaryD5[:])
-plt.plot(time, bufferAMaxBinaryD6[:])
-plt.plot(time, bufferAMaxBinaryD7[:])
+plt.plot(time, bufferDPort0[0])
+plt.plot(time, bufferDPort0[1])
+plt.plot(time, bufferDPort0[2])
+plt.plot(time, bufferDPort0[3])
+plt.plot(time, bufferDPort0[4])
+plt.plot(time, bufferDPort0[5])
+plt.plot(time, bufferDPort0[6])
+plt.plot(time, bufferDPort0[7])
 plt.xlabel('Time (ns)')
 plt.ylabel('Binary')
 plt.show()
