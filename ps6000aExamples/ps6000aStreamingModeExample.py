cd662dc21d34ff215224241369b5e2d42bba4fe8
neil.hamilton <neil.hamilton@picotech.com>
2020-11-02 16:54:26 +0000

    Start ps6000aExamples/ps6000aStreamingModeExample.py

---
+++ ps6000aExamples/ps6000aStreamingModeExample.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2020 Pico Technology Ltd. See LICENSE file for terms.
+#
+# PS6000 A STREAMING MODE EXAMPLE
+# This example opens a 6000a driver device, sets up one channel then collects a streamed set of data.
+# This data is then plotted as mV against time in ns.
+
+import ctypes
+import numpy as np
+from picosdk.ps6000a import ps6000a as ps
+from picosdk.PicoDeviceEnums import picoEnum as enums
+import matplotlib.pyplot as plt
+from picosdk.functions import adc2mV, assert_pico_ok
+
+# Create chandle and status ready for use
+chandle = ctypes.c_int16()
+status = {}
+
+# Open 6000 A series PicoScope
+# returns handle to chandle for use in future API functions
+resolution = enums.PICO_DEVICE_RESOLUTION["PICO_DR_8BIT"]
+status["openunit"] = ps.ps6000aOpenUnit(ctypes.byref(chandle), None, resolution)
+assert_pico_ok(status["openunit"])
+
+# Set channel A on
+# handle = chandle
+channelA = enums.PICO_CHANNEL["PICO_CHANNEL_A"]
+coupling = enums.PICO_COUPLING["PICO_DC"]
+channelRange = 7
+# analogueOffset = 0 V
+bandwidth = enums.PICO_BANDWIDTH_LIMITER["PICO_BW_FULL"]
+status["setChannelA"] = ps.ps6000aSetChannelOn(chandle, channelA, coupling, channelRange, 0, bandwidth)
+assert_pico_ok(status["setChannelA"])
+
+# set channel B-H off
+for x in range (1, 7, 1):
+    channel = x
+    status["setChannel",x] = ps.ps6000aSetChannelOff(chandle,channel)
+    assert_pico_ok(status["setChannel",x])
