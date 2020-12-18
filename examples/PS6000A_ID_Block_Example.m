1cb31f7946a886e325f6b55b65933b2cac205b84
neil.hamilton <neil.hamilton@picotech.com>
2020-12-18 16:31:09 +0000

    Create PS6000A_ID_Block_Example.m

---
+++ examples/PS6000A_ID_Block_Example.m
@@ -0,0 +1,70 @@
+%% PicoScope 6000 Series (A API) Instrument Driver Oscilloscope Block Data Capture Example
+% This is an example of an instrument control session using a device 
+% object. The instrument control session comprises all the steps you 
+% are likely to take when communicating with your instrument. 
+%       
+% These steps are:
+%    
+% # Create a device object   
+% # Connect to the instrument 
+% # Configure properties 
+% # Invoke functions 
+% # Disconnect from the instrument 
+%
+% To run the instrument control session, type the name of the file,
+% PS6000A_ID_Block_Example, at the MATLAB command prompt.
+% 
+% The file, PS6000A_ID_BLOCK_EXAMPLE.M must be on your MATLAB PATH. For
+% additional information on setting your MATLAB PATH, type 'help addpath'
+% at the MATLAB command prompt.
+%
+% *Example:*
+%     PS6000A_ID_Block_Example;
+%
+% *Description:*
+%     Demonstrates how to set properties and call functions in order to
+%     capture a block of data from a PicoScope 6000 (A API) Series Oscilloscope.
+%
+% *See also:* <matlab:doc('icdevice') |icdevice|> | <matlab:doc('instrument/invoke') |invoke|>
+%
+% *Copyright:* © 2020 Pico Technology Ltd. See LICENSE file for terms.
+
+%% Suggested Input Settings
+
+%% Clear Command Window and Close Figures
+
+clc;
+close all;
+
+%% Load Configuration Information
+
+[ps6000aStructs, ps6000aEnumInfo]=PS6000aSetConfig();
+
+%% Device Connection
+
+% Check if an Instrument session using the device object 'ps6000DeviceObj'
+% is still open, and if so, disconnect if the User chooses 'Yes' when prompted.
+if (exist('ps6000aDeviceObj', 'var') && ps6000aDeviceObj.isvalid && strcmp(ps6000aDeviceObj.status, 'open'))
+    
+    openDevice = questionDialog(['Device object ps6000aDeviceObj has an open connection. ' ...
+        'Do you wish to close the connection and continue?'], ...
+        'Device Object Connection Open');
+    
+    if (openDevice == PicoConstants.TRUE)
+        
+        % Close connection to device
+        disconnect(ps6000aDeviceObj);
+        delete(ps6000aDeviceObj);
+        
+    else
+
+        % Exit script if User selects 'No'
+        return;
+        
+    end
+    
+end
+
+%% Create a device object. 
+% The serial number can be specified as a second input parameter.
+ps6000aDeviceObj = icdevice('picotech_ps6000a_generic.mdd','');
