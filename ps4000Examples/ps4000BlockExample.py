ce75bc54ef4e3dbec224c1881d562fcb403345ee
neil.hamilton <neil.hamilton@picotech.com>
2022-02-17 12:19:11 +0000

    Add timebase warning to ps4000 examples

--- ps4000Examples/ps4000BlockExample.py
+++ ps4000Examples/ps4000BlockExample.py
@@ -57,6 +57,8 @@ postTriggerSamples = 2500
 maxSamples = preTriggerSamples + postTriggerSamples
 
 # Get timebase information
+# Warning: When using this example it may not be possible to access all Timebases as all channels are enabled by default when opening the scope.  
+# To access these Timebases, set any unused analogue channels to off.
 # handle = chandle
 # timebase = 8 = timebase
 # noSamples = maxSamples
