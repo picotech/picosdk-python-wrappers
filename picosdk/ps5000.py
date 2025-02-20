458f7b7020387c9adaa20365427788471199d99a
Maxime Roussin-Bélanger <maxime.roussinbelanger@gmail.com>
2025-02-20 11:29:40 -0500

    ps5000.py: Fix incorrect _fields attribute in structure definitions
    Replaced _fields with _fields_ in PWQ_CONDITIONS, TRIGGER_CONDITIONS,
    and
    TRIGGER_CHANNEL_PROPERTIES structure definitions to properly define the
    field
    layout. This ensures correct behavior when interacting with ctypes and
    avoids
    potential attribute errors.

--- picosdk/ps5000.py
+++ picosdk/ps5000.py
@@ -58,7 +58,7 @@ ps5000.PS5000_TIME_UNITS = make_enum([
 
 class PWQ_CONDITIONS (Structure):
 	_pack = 1
-	_fields = [("channelA", c_int32),
+	_fields_ = [("channelA", c_int32),
 				("channelB", c_int32),
 				("channelC", c_int32),
 				("channelD", c_int32),
@@ -69,7 +69,7 @@ ps5000.PWQ_CONDITIONS = PWQ_CONDITIONS
 
 class TRIGGER_CONDITIONS (Structure):
 	_pack = 1
-	_fields = [("channelA", c_int32),
+	_fields_ = [("channelA", c_int32),
 				("channelB", c_int32),
 				("channelC", c_int32),
 				("channelD", c_int32),
@@ -81,7 +81,7 @@ ps5000.TRIGGER_CONDITIONS = TRIGGER_CONDITIONS
 
 class TRIGGER_CHANNEL_PROPERTIES (Structure):
 	_pack = 1
-	_fields = [("thresholdMajor", c_int16),
+	_fields_ = [("thresholdMajor", c_int16),
 				("thresholdMinor", c_int16),
 				("hysteresis", c_uint16),
 				("channel", c_int32),
