13c8650a56d55a42e7c746b386a04a3f1f5de583
JWM <jwm@melexis.com>
2026-03-25 14:00:36 +0100

    Add functionality to find units when not all sdk drivers are installed

--- picosdk/discover.py
+++ picosdk/discover.py
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2018 Pico Technology Ltd. See LICENSE file for terms.
 #
-from picosdk.errors import DeviceNotFoundError
+from picosdk.errors import DeviceNotFoundError, CannotFindPicoSDKError
 from picosdk.ps2000 import ps2000
 from picosdk.ps2000a import ps2000a
 from picosdk.ps3000 import ps3000
@@ -45,6 +45,23 @@ def find_all_units():
         try:
             device = driver.open_unit()
         except DeviceNotFoundError:
+            continue
+        devices.append(device)
+    if not devices:
+        raise DeviceNotFoundError("Could not find any devices on any drivers.")
+    return devices
+
+
+def find_units_safely():
+    """Search for, open and return ALL devices on ALL pico drivers (supported in this SDK wrapper).
+    Ignores CannotFindPicoSDKErrors."""
+    devices = []
+
+    for driver in drivers:
+        try:
+            device = driver.open_unit()
+        except (CannotFindPicoSDKError, DeviceNotFoundError):
+            # Go to next driver when sdk is not installed or device is not found
             continue
         devices.append(device)
     if not devices:
